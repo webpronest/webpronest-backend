2913c76a2f1926eddd80421e24db0f1a7a5edf44
Vijay Shankar Vishwakarma <avshankar9151@gmail.com>
2026-01-04 06:01:38 +0530

    code fixes

--- app/config.py
+++ app/config.py
@@ -1,13 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL: str = ''
     DEBUG: bool = False
-    GOOGLE_CLIENT_ID: str
-    GOOGLE_CLIENT_SECRET: str
-    GOOGLE_REDIRECT_URI: str
-    JWT_SECRET: str
-    JWT_ALGORITHM: str
+    GOOGLE_CLIENT_ID: str = ''
+    GOOGLE_CLIENT_SECRET: str = ''
+    GOOGLE_REDIRECT_URI: str = ''
+    JWT_SECRET: str = ''
+    JWT_ALGORITHM: str = ''
 
     model_config = SettingsConfigDict(env_file=".env")
 
